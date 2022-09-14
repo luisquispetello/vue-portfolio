3378e94d1c39c656639d66a790a77f93cf783e69
Luis Quispe Tello <luisquispetello@gmail.com>
2022-09-14 22:12:53 +0200

    update notfound

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,12 +2,12 @@
   <div>
     <h2>Not found</h2>
     <p>Sorry, the page you are searching for does not exist</p>
-    <NavMenu />
+    <BackButton />
   </div>
 </template>
 
 <script setup>
-import NavMenu from "../components/NavMenu.vue";
+import BackButton from "../components/BackButton.vue";
 </script>
 
 <style scoped></style>
