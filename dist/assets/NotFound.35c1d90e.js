fc6fb873c216cb4187aa3ef15be40c28227bcf99
Luis Quispe Tello <luisquispetello@gmail.com>
2022-09-14 17:29:23 +0200

    css update

---
+++ dist/assets/NotFound.35c1d90e.js
@@ -0,0 +1,1 @@
+import{N as o}from"./NavMenu.ccdd20bb.js";import{o as t,d as a,e as r,a as e}from"./index.1055c6cb.js";const s=e("h2",null,"Not found",-1),n=e("p",null,"Sorry, the page you are searching for does not exist",-1),p={__name:"NotFound",setup(c){return(_,u)=>(t(),a("div",null,[s,n,r(o)]))}};export{p as default};
