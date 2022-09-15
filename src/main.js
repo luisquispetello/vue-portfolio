ecdd45adbf1db790ad624148bc9962f8d563c7eb
Luis Quispe Tello <luisquispetello@gmail.com>
2022-09-15 19:14:16 +0200

    fix error displaying images

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './assets/css/style.css'
 import App from './App.vue'
 import router from './router'
 import { MotionPlugin } from '@vueuse/motion'
+import VueGtag from 'vue-gtag'
 
 
 const app = createApp(App)
 app.use(router)
 app.use(MotionPlugin)
+app.use(VueGtag, {
+  config: { id: "G-7X8SJCVBFY" }
+})
 app.mount('#app')
