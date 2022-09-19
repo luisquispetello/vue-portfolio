6cfd5562025f33fbf7ce30999bafeded56eb08e2
Luis Quispe Tello <luisquispetello@gmail.com>
2022-09-19 11:21:35 +0200

    update img source

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
-import { createApp } from 'vue'
-import './assets/css/style.css'
-import App from './App.vue'
-import router from './router'
-import { MotionPlugin } from '@vueuse/motion'
-import VueGtag from 'vue-gtag'
+import { createApp } from "vue";
+import "./assets/css/style.css";
+import App from "./App.vue";
+import router from "./router";
+import { MotionPlugin } from "@vueuse/motion";
+import VueGtag from "vue-gtag";
 
-
-const app = createApp(App)
-app.use(router)
-app.use(MotionPlugin)
-app.use(VueGtag, {
-  config: { id: "G-7X8SJCVBFY" }
-})
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(MotionPlugin);
+app.use(
+  VueGtag,
+  {
+    appName: "My portfolio",
+    pageTrackerScreenviewEnabled: true,
+    config: { id: "G-7X8SJCVBFY" },
+  },
+  router
+);
+app.mount("#app");
